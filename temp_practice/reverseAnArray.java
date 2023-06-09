5876b7e359565243f6e2e50c958958b936d12131
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-06-09 15:35:05 +0530

    practice reverse an array

---
+++ temp_practice/reverseAnArray.java
@@ -0,0 +1,23 @@
+package temp_practice;
+
+public class reverseAnArray {
+    static int[] reverseArray(int []arr){
+        int idx = 0;
+        int rev[] = new int[arr.length];
+        for(int i = arr.length-1; i>=0; i--){
+            rev[idx] = arr[i];
+            idx++;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        int ar[] = {1,2,4,5,6,3,5,0,6,34,6,23,56};
+
+        int result[] = reverseArray(ar);
+        System.out.print("reverse: ");
+        for (int i = 0; i < result.length; i++)
+            System.out.print(result[i] + " ");
+    }
+}
+
