0eff2e8d98729f7ce79759022920ea0410ee212b
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-06-10 13:19:58 +0530

    Scanner sc error

--- temp_practice/resizeArray.java
+++ temp_practice/resizeArray.java
@@ -15,13 +15,13 @@ public class resizeArray {
     public static void main(String[] args) {
 
         int ar[] = {1,2,4,5,6,3,5,0,6,34,6,23,56};
-        Scanner sc=new Scanner(System.in); 
-        
-        System.out.println("Enter the Capacity of new array ");
-        int cap = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the Capacity of new array ");
+            int cap = sc.nextInt();
 
-        int result[] = resizeArrays(ar, cap);
-        for (int i = 0; i < result.length; i++)
-            System.out.print(result[i] + " ");
+            int result[] = resizeArrays(ar, cap);
+            for (int i = 0; i < result.length; i++)
+                System.out.print(result[i] + " ");
+        }
     }
 }
