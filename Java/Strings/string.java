5612019a9eaaf96c555271f3a14ad80c01ed46b5
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-06-11 00:27:36 +0530

    new DS strings

---
+++ Java/Strings/string.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class string {
+    static boolean isPalindrome(String str){
+        char[] charArray = str.toCharArray();
+        int start = 0;
+        int end = str.length() - 1;
+        while (start<end){
+            if(charArray[start] != charArray[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+   }
+
+   public static void main(String[] args) {
+    String string = "madam";
+    boolean result = isPalindrome(string);
+    System.out.println(result);
+   }
+}
