d3f21f28997373e6dac19439f78aae11b7e80543
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-06-10 23:16:06 +0530

    practice find missing number in an array

---
+++ temp_practice/missingNum.java
@@ -0,0 +1,18 @@
+package temp_practice;
+
+public class missingNum {
+    static int missingNumber(int n, int[] arr){
+        int sum1 = (n*(n+1))/2;
+        int sum2 = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum2 += arr[i];
+        }
+        return sum1 - sum2;
+    }
+    public static void main(String[] args) {
+        int ar[] = {1,2,3,5};
+
+        int result = missingNumber(5, ar);
+        System.out.println(result);
+    }
+}
