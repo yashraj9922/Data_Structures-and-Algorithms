2bbe41b23d1af3c8a1248487b5d1da9ca6bbfbad
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-07-20 23:59:50 +0530

    selectionSort

---
+++ Sorting/selectionSort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        // int[] arr = { 5, 1, 4, 2, 8 };
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of an Array: ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter elements of an array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+        int n = arr.length;
+        selection(arr, n);
+        printArray(arr, n);
+    }
+
+    static void selection(int[] arr, int n) {
+        for(int i = 0; i < arr.length - 1; i++){
+            int smallest = i;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+        int temp = arr[smallest];
+        arr[smallest] = arr[i];
+        arr[i] = temp;
+        }
+    }
+
+    static void printArray(int[] arr, int n) {
+        int i;
+        System.out.println("Non-Decreasing order: ");
+        for (i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+            // System.out.println();
+        }
+    }
+}
