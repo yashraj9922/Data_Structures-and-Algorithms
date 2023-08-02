372d00e60292c074f742cd288530c8cdbabd01c0
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-02 11:12:57 +0530

    linearsearch in Strings

--- Searching/linearSearch.java
+++ Searching/linearSearch.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 
 public class linearSearch {
 
+    //search element in array: return the index if item found
+    //otherwise if item not found return -1
     static int linearsearch(int arr[], int key){
         if(arr.length == 0){
             return -1;
         }
 
-        for(int i = 0; i < arr.length; i++){
-            int element = arr[i];
+        for(int idx = 0; idx < arr.length; idx++){
+            int element = arr[idx];
             if(element == key){
-                return i;
+                return idx;
             }
         }
-        return -1;
+        return -1; //if element not found
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
