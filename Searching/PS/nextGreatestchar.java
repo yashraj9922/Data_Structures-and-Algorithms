e3b821fcab8654ff1c3b577ac95f98963a20c913
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-07 18:38:35 +0530

    Leet Code que

---
+++ Searching/PS/nextGreatestchar.java
@@ -0,0 +1,18 @@
+package PS;
+
+public class nextGreatestchar {
+    public char nextGreatestCharacter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % (letters.length - 1)];
+    }
+}
