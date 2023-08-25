58d0e9e21749279853e884522463ca7795870d51
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-25 11:44:56 +0530

    Leetcode

--- Arrays/PS/concatenationArray.java
+++ Arrays/PS/concatenationArray.java
@@ -1,11 +1,11 @@
 package PS;
- // Leetcode 1929. Concatenation of Array
+// Leetcode 1929. Concatenation of Array
 
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length;
-        int []arr = new int[2*n];
-        for(int i = 0; i < n; i++){
+        int[] arr = new int[2 * n];
+        for (int i = 0; i < n; i++) {
             arr[i] = nums[i];
             arr[i + n] = nums[i];
         }
@@ -16,9 +16,9 @@ class Solution {
 public class concatenationArray {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int []nums = {1,2,1};
-        int []arr = s.getConcatenation(nums);
-        for(int i = 0; i < arr.length; i++){
+        int[] nums = { 1, 2, 1 };
+        int[] arr = s.getConcatenation(nums);
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
--- Arrays/PS/shuffleArray.java
+++ Arrays/PS/shuffleArray.java
@@ -3,11 +3,11 @@ package PS;
 // Leetcode 1470. Shuffle the Array
 
 class Solution {
-    public int[] shuffle(int[] nums, int n){
-        int []arr = new int[2*n];
-        for(int i = 0; i < n; i++){
-            arr[2*i] = nums[i];
-            arr[2*i + 1] = nums[i + n];
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            arr[2 * i] = nums[i];
+            arr[2 * i + 1] = nums[i + n];
         }
         return arr;
     }
@@ -16,10 +16,10 @@ class Solution {
 public class shuffleArray {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int []nums = {2,5,1,3,4,7};
+        int[] nums = { 2, 5, 1, 3, 4, 7 };
         int n = 3;
-        int []arr = s.shuffle(nums, n);
-        for(int i = 0; i < arr.length; i++){
+        int[] arr = s.shuffle(nums, n);
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
