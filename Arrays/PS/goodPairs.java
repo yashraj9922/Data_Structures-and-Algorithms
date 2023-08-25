75ce7a49389800b213f20ac972c93bd1d156ab21
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-25 12:03:31 +0530

    leetcode good pairs

---
+++ Arrays/PS/goodPairs.java
@@ -0,0 +1,24 @@
+// 1512. Number of Good Pairs
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (current == nums[j]) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
+
+public class goodPairs {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = { 1, 2, 3, 1, 1, 3 };
+        System.out.println(s.numIdenticalPairs(nums));
+    }
+}
