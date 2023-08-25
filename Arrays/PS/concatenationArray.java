b06ae19d8c63296a7d1f3273d75048fc40c8e98f
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-25 11:43:47 +0530

    concatenation of Array

---
+++ Arrays/PS/concatenationArray.java
@@ -0,0 +1,25 @@
+package PS;
+ // Leetcode 1929. Concatenation of Array
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int []arr = new int[2*n];
+        for(int i = 0; i < n; i++){
+            arr[i] = nums[i];
+            arr[i + n] = nums[i];
+        }
+        return arr;
+    }
+}
+
+public class concatenationArray {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int []nums = {1,2,1};
+        int []arr = s.getConcatenation(nums);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
