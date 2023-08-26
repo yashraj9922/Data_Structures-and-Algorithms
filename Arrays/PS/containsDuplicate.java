051af515c74035b4a417f6b0cd18b144486f6192
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-26 10:36:47 +0530

    LC 217

---
+++ Arrays/PS/containsDuplicate.java
@@ -0,0 +1,25 @@
+// Leetcode 217. Contains Duplicate
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int counter = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[counter]) {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+        return counter != nums.length - 1;
+    }
+}
+
+public class containsDuplicate {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = { 1, 2, 3, 1 };
+        System.out.println(s.containsDuplicate(nums));
+    }
+}
