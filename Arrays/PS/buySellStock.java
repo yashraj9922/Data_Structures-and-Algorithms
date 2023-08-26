751aa4f47dba37a8a72f27bc7342a0c837dd4fdf
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-26 11:26:54 +0530

    LC 121

---
+++ Arrays/PS/buySellStock.java
@@ -0,0 +1,26 @@
+// Leetcode 121 - Best Time to Buy and Sell Stock
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min)
+                min = prices[i];
+
+            profit = prices[i] - min;
+            if (profit > maxProfit)
+                maxProfit = profit;
+        }
+        return maxProfit;
+    }
+}
+
+public class buySellStock {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(s.maxProfit(prices));
+    }
+}
