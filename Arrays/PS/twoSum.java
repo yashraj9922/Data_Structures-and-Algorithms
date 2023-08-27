20df816ec53e7121218256260a02d0ddad0bda7b
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-27 23:57:31 +0530

    test case added and solved

--- Arrays/PS/twoSum.java
+++ Arrays/PS/twoSum.java
@@ -1,9 +1,11 @@
 // Leetcode 1. Two Sum
 
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
 public class twoSum {
     public static void main(String[] args) {
-        int[] nums = { 2, 7, 11, 15 };
-        int target = 9;
+        int[] nums = { 3, 4, 2 };
+        int target = 6;
 
         int[] result = sum(nums, target);
         System.out.println(result[0] + " " + result[1]);
@@ -18,7 +20,7 @@ public class twoSum {
         }
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < temp.length; j++) {
+            for (int j = i + 1; j < temp.length; j++) {
                 if (nums[i] == temp[j]) {
                     result[0] = i;
                     result[1] = j;
