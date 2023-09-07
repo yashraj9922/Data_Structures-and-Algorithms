7ca92bca200cc342077342689c9d597ea434347a
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-09-07 15:51:53 +0530

    LC 14

---
+++ Strings/LC/longestCommonPrefix.java
@@ -0,0 +1,26 @@
+package LC;
+
+// 14. Longest Common Prefix
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+        String prefix = strs[0];
+        for(int i = 0; i < strs.length; i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+}
+
+public class longestCommonPrefix {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(s.longestCommonPrefix(strs));
+    }
+}
