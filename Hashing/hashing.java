41a94bb66f779d2fc6e21a0ad2b073c50ce0c555
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-04 00:15:38 +0530

    hashset

---
+++ Hashing/hashing.java
@@ -0,0 +1,25 @@
+package Hashing;
+
+import java.util.HashSet;
+
+public class hashing {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(1);
+
+        System.out.println(set);
+
+        set.remove(1);
+
+        System.out.println(set);
+
+        boolean x = set.contains(4);
+
+        System.out.println(x);
+    }
+}
