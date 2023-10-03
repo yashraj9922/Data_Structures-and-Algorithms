9688b2de7050f3d268200811d398d11d17ca3ecd
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-04 00:59:28 +0530

    hash maps

---
+++ Hashing/hashmaps.java
@@ -0,0 +1,18 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class hashmaps {
+    public static void main(String[] args) {
+        HashMap map = new HashMap<>();
+
+        map.put("India", 100);
+        map.put("US", 90);
+        map.put("UK", 80);
+        map.put("China", 100);
+
+        System.out.println(map);
+
+    }
+
+}
--- Hashing/hashsets.java
+++ Hashing/hashsets.java
@@ -2,7 +2,7 @@ package Hashing;
 
 import java.util.HashSet;
 
-public class hashing {
+public class hashsets {
     public static void main(String[] args) {
         HashSet<Integer> set = new HashSet<>();
 
