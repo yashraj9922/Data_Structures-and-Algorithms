d83542d9d2d75ffd2e26bd6d21a8d1b5aea7e823
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-13 23:49:28 +0530

    ! swapped condition

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -20,14 +20,19 @@ public class bubbleSort {
     }
 
     static void bubble(int[] arr) {
+        boolean swapped;
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
+                    swapped = true;
                 }
             }
+            if (!swapped)
+                break;
         }
     }
 
