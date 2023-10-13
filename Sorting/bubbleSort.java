536f2289b2febcc099b45d6331550c6d67fff8a7
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-13 23:43:44 +0530

    bubble sort

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -15,27 +15,26 @@ public class bubbleSort {
             System.out.print("Element " + (i + 1) + ": ");
             arr[i] = sc.nextInt();
         }
-        int n = arr.length;
-        bubble(arr, n);
-        printArray(arr, n);
+        bubble(arr);
+        printArray(arr);
     }
 
-    static void bubble(int[] arr, int n) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
+    static void bubble(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 }
             }
         }
     }
 
-    static void printArray(int[] arr, int n) {
+    static void printArray(int[] arr) {
         int i;
         System.out.println("Non-Decreasing order: ");
-        for (i = 0; i < n; i++) {
+        for (i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
             // System.out.println();
         }
