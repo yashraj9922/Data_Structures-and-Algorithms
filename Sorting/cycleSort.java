d2eb9abb1da9bd45c3cdcd78786c77a497dd9c5b
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-22 21:27:59 +0530

    cycle sort

---
+++ Sorting/cycleSort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class cycleSort {
+    static int[] cycle(int arr[]){
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                int temp = arr[correct];
+                arr[correct] = arr[i];
+                arr[i] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of an Array: ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter elements of an array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
