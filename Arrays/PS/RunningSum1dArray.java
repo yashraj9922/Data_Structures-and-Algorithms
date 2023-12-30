d56bdc3d0b41ec8fa8e642e0b94db886cf46d8ce
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-12-30 14:15:15 +0530

    1480 LC

---
+++ Arrays/PS/RunningSum1dArray.java
@@ -0,0 +1,17 @@
+
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// leetcode: https://leetcode.com/problems/running-sum-of-1d-array/
+// 1480. Running Sum of 1d Array
+
+public class RunningSum1dArray {
+    class Solution {
+        public int[] runningSum(int[] nums) {
+
+            for (int i = 1; i < nums.length; i++) {
+                nums[i] = nums[i - 1] + nums[i];
+
+            }
+            return nums;
+        }
+    }
+}
