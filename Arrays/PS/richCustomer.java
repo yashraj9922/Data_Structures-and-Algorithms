138dd6b611df5bc70904b7da57a11cecad29c599
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-12-30 16:12:12 +0530

    LC 1672

---
+++ Arrays/PS/richCustomer.java
@@ -0,0 +1,18 @@
+public class richCustomer {
+    // 1672. Richest Customer Wealth
+    // leetcode: https://leetcode.com/problems/richest-customer-wealth/
+
+    class Solution {
+        public int maximumWealth(int[][] accounts) {
+            int maxSalary = 0;
+            for (int i = 0; i < accounts.length; i++) {
+                int tempSalary = 0;
+                for (int j = 0; j < accounts[i].length; j++) {
+                    tempSalary += accounts[i][j];
+                }
+                maxSalary = Math.max(maxSalary, tempSalary);
+            }
+            return maxSalary;
+        }
+    }
+}
