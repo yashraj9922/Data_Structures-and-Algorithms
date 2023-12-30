44206ae5ddc67a2475eb1be567f3d639f5aa5280
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-12-30 16:32:49 +0530

    1431 LC

--- Arrays/PS/RunningSum1dArray.java
+++ Arrays/PS/RunningSum1dArray.java
@@ -1,4 +1,3 @@
-
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 // leetcode: https://leetcode.com/problems/running-sum-of-1d-array/
 // 1480. Running Sum of 1d Array
---
+++ Arrays/PS/kidsCandiesGreatest.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// 1431. Kids With the Greatest Number of Candies
+// link: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+public class kidsCandiesGreatest {
+    class Solution {
+        public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+            int maxCandies = 0;
+            ArrayList<Boolean> result = new ArrayList<Boolean>(candies.length);
+            // int arr[] = new int[candies.length];
+            for (int i = 0; i < candies.length; i++) {
+                maxCandies = Math.max(maxCandies, candies[i]);
+            }
+            for (int i = 0; i < candies.length; i++) {
+                candies[i] += extraCandies;
+                if (candies[i] >= maxCandies) {
+                    result.add(true);
+                } else {
+                    result.add(false);
+                }
+            }
+            return result;
+        }
+    }
+}
